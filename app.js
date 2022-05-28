543c4e9b63cce57e1d5d296ad1fedb8898aa45c8
Mahnaz <md.jazayeri.sev@gmail.com>
2022-05-28 13:41:10 +0200

    added a preloader

--- app.js
+++ app.js
@@ -49,3 +49,27 @@ search.addEventListener('keyup', () => {
     const term = search.value.trim().toLowerCase();
     filterTodos(term);
 });
+
+
+let loader = document.querySelector('#loader');
+
+function loadNow(opacity) {
+    if (opacity <= 0) {
+        displayContent();
+    } else {
+        loader.style.opacity = opacity;
+        window.setTimeout(function() {
+            loadNow(opacity - 0.04);
+        }, 50);
+    }
+}
+
+function displayContent() {
+    loader.style.display = 'none';
+    document.getElementById('content').style.display = 'block';
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    loader = document.getElementById('loader');
+    loadNow(1);
+});
